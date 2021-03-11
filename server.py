b1507a5f18c08a00c0b8c9e535e9e1df7078a2db
Tomás <80177664+TomasVPerez@users.noreply.github.com>
2021-03-11 01:31:52 -0300

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import threading
 import socket
 
 ip = "127.0.0.1" #La ip que si solo es en casa se usa localhost
-puerto = 55555
+puerto = 44444
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((ip, puerto))
@@ -20,11 +20,11 @@ def controlar(usuario):
         try:
             mensaje = usuario.recv(1024)
             enviarATodos(mensaje) #recibimos mensajes de los usuarios y los enviamos a todos los demas usuarios
-        except: #si hay un error al recibir el mensaje o conexion con el usuario cortamos la conexion y lo removemos de la lista
-            indice = usuarios.index(usuario) #conseguimos su posicion en la lista
+        except: 
+            indice = usuarios.index(usuario) 
             usuarios.remove(usuario)
             usuario.close() 
-            nickname = nicknames[indice] #lo buscamos con ese indice en la lista de nicknames y lo borramos
+            nickname = nicknames[indice] 
             enviarATodos(f'{nickname} abandonó el chat'.encode('ascii'))
             nicknames.remove(nickname)
             break
@@ -32,23 +32,23 @@ def controlar(usuario):
 def recibir():
     while True:
         usuario, ip = server.accept() #acepta las conexiones de cualquier usuario
-        print(f'Conectado desde: {str(ip)}') #me dice desde donde se conectaron
+        print(f'Conectado desde: {str(ip)}') 
 
-        usuario.send('NICK'.encode('ascii')) #le envio una palabra clave para esperar una respuesta especifica(su nick en este caso)
-        nickname = usuario.recv(1024).decode('ascii') #recibo el nickname
-        nicknames.append(nickname) #lo agrego a la lista de nicks
-        usuarios.append(usuario) #lo agrego a la lsta de usuarios
+        usuario.send('NICK'.encode('ascii')) 
+        nickname = usuario.recv(1024).decode('ascii') 
+        nicknames.append(nickname) 
+        usuarios.append(usuario) 
 
-        print(f'El nickname del usuario es {nickname}') #me muestra el nick q eligio
+        print(f'El nickname del usuario es {nickname}') 
         enviarATodos(f'{nickname} se unio al chat\n'.encode('ascii')) 
-        usuario.send("Conectado al servidor".encode('ascii')) #le mando un mensaje a ese usuario
+        usuario.send("Conectado al servidor".encode('ascii')) 
 
         thread = threading.Thread(target=controlar, args=(usuario,)) #inicio un thread porque necesito controlar muchos clientes/trafico a la vez
         thread.start()  
 
     
 print("Esperando conexiones...")
-recibir() #corro el main
+recibir() 
 
 
 
